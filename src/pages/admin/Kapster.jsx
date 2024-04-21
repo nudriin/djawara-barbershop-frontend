fb25d2bd8a6a9ec0772eb7f120acb389082f7616
Nurdin <mrsunnysummer@gmail.com>
2024-04-21 21:46:53 +0700

    Create kapsters admin page functionality

--- src/pages/admin/Kapster.jsx
+++ src/pages/admin/Kapster.jsx
@@ -1,29 +1,50 @@
 import { NavLink } from "react-router-dom";
 import AdminLayout from "../../components/AdminLayout";
+import { useEffect, useState } from "react";
 
 export default function Kapster() {
+    const [kapsters, setKapsters] = useState([]);
+
+
+
+    useEffect(() => {
+        const getKapsters = async () => {
+            try {
+                const response = await fetch("/api/v1/kapsters", {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                const data = await response.json();
+                setKapsters(data.data);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        getKapsters();
+    }, []);
+
     return (
         <AdminLayout>
-            <div className="flex flex-col gap-5 items-center justify-center w-9/12 mx-auto">
-                <div className="text-center space-y-4">
-                    <h1 className="text-4xl text-white font-futura mb-4">Kapster</h1>
+            <div className="flex flex-col items-center justify-center w-9/12 gap-5 mx-auto">
+                <div className="space-y-4 text-center">
+                    <h1 className="mb-4 text-4xl text-white font-futura">Kapster</h1>
                     <NavLink to="/admin/kapsters/add">
-                        <button className="text-slate-900 bg-lime px-2 py-2 rounded-full">Tambah</button>
+                        <button className="px-2 py-2 rounded-full text-slate-900 bg-lime">Tambah</button>
                     </NavLink>
                 </div>
                 <div className="flex flex-wrap items-center justify-center gap-10 pb-10">
-                    <div className="max-w-sm overflow-hidden text-center hover:bg-purple rounded-full bg-lime text-slate-900 hover:text-white">
-                        <img src="https://placehold.co/250x250" className="object-cover object-top w-[250px] h-[250px]" />
-                    </div>
-                    <div className="max-w-sm overflow-hidden text-center hover:bg-purple rounded-full bg-lime text-slate-900 hover:text-white">
-                        <img src="https://placehold.co/250x250" className="object-cover object-top w-[250px] h-[250px]" />
-                    </div>
-                    <div className="max-w-sm overflow-hidden text-center hover:bg-purple rounded-full bg-lime text-slate-900 hover:text-white">
-                        <img src="https://placehold.co/250x250" className="object-cover object-top w-[250px] h-[250px]" />
-                    </div>
-                    <div className="max-w-sm overflow-hidden text-center hover:bg-purple rounded-full bg-lime text-slate-900 hover:text-white">
-                        <img src="https://placehold.co/250x250" className="object-cover object-top w-[250px] h-[250px]" />
-                    </div>
+                    {kapsters.map((value, index) => (
+                        <div className="flex flex-col items-center justify-center gap-2" key={index}>
+                            <div className="max-w-sm overflow-hidden text-center rounded-full hover:bg-purple bg-lime text-slate-900 hover:text-white"  >
+                                <img src={value.profile_pic} className="object-cover object-top w-[250px] h-[250px]" />
+                            </div>
+                            <p className="text-white">{value.name}</p>
+                        </div>
+                    ))}
+
                 </div>
             </div>
         </AdminLayout>
